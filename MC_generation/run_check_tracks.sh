c311ca4b15637a9b1387233977e14847a75f97ab
Fernando Torales Acosta <ftorales@lxplus740.cern.ch>
2021-05-20 22:13:00 +0200

    fixed some file paths

--- MC_generation/run_check_tracks.sh
+++ MC_generation/run_check_tracks.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 job_id=$1
 n_subjobs=$2
-dir="/eos/user/f/ftorales/its_data/alignment/lbnl_alice_alignment_tasks/MC_genertion/"
+dir="/eos/user/f/ftorales/its_data/alignment/lbnl_alice_alignment_tasks"
 
 for ((i=0; i<n_subjobs; i++))
 do
   echo "i: $i"
-  cp $dir/macros/CheckTracks.C $dir/$job_id/$i/
+  cp $dir/macros/CheckTracks.C $dir/MC_generation/$job_id/$i/
   cd $dir/$job_id/$i/
   root -b -l -q CheckTracks.C
-  cd $dir/ 
+  cd $dir/MC_generation/
 done
 
 hadd -k -f ${job_id}_Check_Tracks.root ${dir}/${job_id}/*/CheckTracks.root
